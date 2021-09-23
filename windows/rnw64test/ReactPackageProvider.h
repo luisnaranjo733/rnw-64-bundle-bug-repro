4441ecb524c47b389c6e04e70a73c0255f474daa
Luis Naranjo Gomez <jonaranj@microsoft.com>
2021-09-23 15:33:48 -0700

    proj skel

---
+++ windows/rnw64test/ReactPackageProvider.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+namespace winrt::rnw64test::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::rnw64test::implementation
+
